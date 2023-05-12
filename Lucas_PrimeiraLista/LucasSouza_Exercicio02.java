2374b73726804a6206ddc42a7b5e1a38dc41971b
Diablo <82063404+LucasDiabloChan@users.noreply.github.com>
2023-05-12 14:16:56 -0300

    Update and rename exercicio2.java to LucasSouza_Exercicio02.java

---
+++ Lucas_PrimeiraLista/LucasSouza_Exercicio02.java
@@ -0,0 +1,8 @@
+// Todo seu código deve estar dentro dessa classe
+public class LucasSouza_Exercicio01{
+  
+  // Todo seu código deve ser escrito/chamado aqui
+  public static void main(String args[]){
+    System.out.println("Aqui ó abextado!");
+  }
+}
