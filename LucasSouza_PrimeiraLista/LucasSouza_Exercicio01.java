f037860877bbae600b9cb8018412eef5d7962415
Diablo <82063404+LucasDiabloChan@users.noreply.github.com>
2023-05-29 16:57:13 -0300

    Update LucasSouza_Exercicio01.java

--- LucasSouza_PrimeiraLista/LucasSouza_Exercicio01.java
+++ LucasSouza_PrimeiraLista/LucasSouza_Exercicio01.java
@@ -3,6 +3,27 @@ public class LucasSouza_Exercicio01{
   
   // Todo seu código deve ser escrito/chamado aqui
   public static void main(String[] args){
-    System.out.println("Aqui ó abextado!");
+    		/* CRIAÇÃO DAS VARIÁVEIS */
+            Scanner reader;
+            int num, nois, nes, natro, soma;
+      
+		    /* INICIALIZAÇÃO DAS VARIÁVEIS */
+            reader = new Scanner(System.in);
+            num = nois = nes = natro = soma = 0;
+        
+        /* PEGANDO VALORES */
+            System.out.System.out.println("Digite quatro números: ");
+                num   = reader.nextInt();
+                nois  = reader.nextInt();
+                nes   = reader.nextInt();
+                natro = reader.nextInt();
+        
+        /* SOMA OS QUATRO NÚMEROS */
+            soma = num + nois + nes + natro;
+            
+        /* EXIBE O RESULTADO */
+            System.out.println("O resultado é: " + soma);
+        
+        
   }
 }
