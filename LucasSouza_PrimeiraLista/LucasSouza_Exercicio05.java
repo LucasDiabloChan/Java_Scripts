9f82c72b341fe5a8b1fe4f424eb019cdd0f9970f
Diablo <82063404+LucasDiabloChan@users.noreply.github.com>
2023-06-09 17:06:32 -0300

    Create LucasSouza_Exercicio05.java
    Correção do exercício 05

---
+++ LucasSouza_PrimeiraLista/LucasSouza_Exercicio05.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class LucasSouza_Exercicio05{
+    public static void main(String[] args){
+        * CRIAÇÃO DAS VARIÁVEIS */
+          Scanner read = new Scanner(System.in);
+          short idade;
+        
+        /* INICIALIZAÇÃO DAS VARIÁVEIS */
+          idade = 0;
+        
+        /* RECEBENDO OS VALORES */
+          System.out.println("Insira sua idade: ");
+            idade = read.nextShort();
+            
+        /* EXIBE A CONVERSÃO */
+            if(idade >= 65)
+                System.out.println("Tu é idoso.");
+            else if(idade >= 19)
+                System.out.println("Tu é adulto.");
+            else if(idade >= 12)
+                System.out.println("Tu é adolescente.");
+            else if(idade >= 0)
+                System.out.println("Tu é um bebê.");
+            else
+                System.out.println("Para de mentir macho, tu tem " + idade + " anos?");    
+    }
+}
