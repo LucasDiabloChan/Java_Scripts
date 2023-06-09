e540a0afa39a60fcb2db039e0025a11409263098
Diablo <82063404+LucasDiabloChan@users.noreply.github.com>
2023-06-09 17:25:35 -0300

    Update LucasSouza_Exercicio01.java
    Resolução do exercício 01.

--- LucasSouza_SegundaLista/LucasSouza_Exercicio01.java
+++ LucasSouza_SegundaLista/LucasSouza_Exercicio01.java
@@ -3,6 +3,20 @@ public class LucasSouza_Exercicio01{
   
   // Todo seu código deve ser escrito/chamado aqui
   public static void main(String[] args){
-    System.out.println("Aqui ó abextado!");
+      /* CRIAÇÃO DAS VARIÁVEIS */
+          Scanner read = new Scanner(System.in);
+          float desconto, valor;
+        
+      /* INICIALIZAÇÃO DAS VARIÁVEIS */
+          valor = 0.0f;
+          desconto = 0.85f; // 1 - (15 / 100) = 0.85
+        
+      /* RECEBENDO OS VALORES */
+          System.out.println("Insira o valor a ser descontado: ");
+          valor = read.nextFloat();
+            
+      /* EXIBE O RESULTADO */
+          System.out.println("Sem desconto: " + (valor));
+          System.out.println("Com desconto: " + (valor * desconto));
   }
 }
