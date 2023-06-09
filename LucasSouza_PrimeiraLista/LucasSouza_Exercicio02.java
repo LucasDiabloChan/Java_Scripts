a981c5a4ea822cab0179dbe61358ec5f2525e5ae
Diablo <82063404+LucasDiabloChan@users.noreply.github.com>
2023-06-09 16:36:49 -0300

    Update LucasSouza_Exercicio02.java
    Postagem de uma possível solução do exercício 2

--- LucasSouza_PrimeiraLista/LucasSouza_Exercicio02.java
+++ LucasSouza_PrimeiraLista/LucasSouza_Exercicio02.java
@@ -1,8 +1,30 @@
+// Chama a classe Scanner
+import java.util.Scanner;
+
 // Todo seu código deve estar dentro dessa classe
 public class LucasSouza_Exercicio01{
   
   // Todo seu código deve ser escrito/chamado aqui
   public static void main(String args[]){
-    System.out.println("Aqui ó abextado!");
+    
+    /* CRIAÇÃO DAS VARIÁVEIS */
+      Scanner reader = new Scanner(System.in);
+      int valor1, valor2, valor3, valor4, soma;
+    
+    /* INICIALIZAÇÃO DAS VARIÁVEIS */
+      valor1 = valor2 = valor3 = valor4 = soma = 0;
+    
+    /* RECEBE OS VALORES */
+      System.out.println("Insira quatro valores inteiros: ");
+      valor1 = reader.nextInt();
+      valor2 = reader.nextInt();
+      valor3 = reader.nextInt();
+      valor4 = reader.nextInt();
+    
+    /* CALCULA A SOMA DOS (x²) */
+      soma = (valor1 * valor1) + (valor2 * valor2) + (valor3 * valor3) + (valor4 * valor4);
+    
+    /* EXIBE A SOMA */
+      System.out.println("O resultado da soma dos quadrados é: " + soma);
   }
 }
