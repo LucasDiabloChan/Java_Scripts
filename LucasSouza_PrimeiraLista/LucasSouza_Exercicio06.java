7d9942e8acdefaccc2c23699cee32c07beb04cb2
Diablo <82063404+LucasDiabloChan@users.noreply.github.com>
2023-06-09 17:16:22 -0300

    Create LucasSouza_Exercicio06.java

---
+++ LucasSouza_PrimeiraLista/LucasSouza_Exercicio06.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class LucasSouza_Exercicio06{
+    public static void main (String[] args){
+        /* CRIAÇÃO DAS VARIÁVEIS */
+            Scanner read = new Scanner(System.in);
+            float raio;
+            double doisPi;
+        
+        /* INICIALIZAÇÃO DAS VARIÁVEIS */
+            raio = 0f;
+            doisPi = Math.PI * 2;
+
+        /* RECEBENDO OS VALORES */
+            System.out.println("Insira o raio: ");
+            raio = read.nextFloat();
+            
+        /* EXIBE O RESULTADO */
+            System.out.println("O perímetro mede: " + (doisPi * raio));
+    }
+}
