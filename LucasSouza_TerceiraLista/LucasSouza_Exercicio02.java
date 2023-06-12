f34d4b556dc539a36f10e2479bf53898901b57b9
Diablo <82063404+LucasDiabloChan@users.noreply.github.com>
2023-06-12 09:26:01 -0300

    Create LucasSouza_Exercicio02.java

---
+++ LucasSouza_TerceiraLista/LucasSouza_Exercicio02.java
@@ -0,0 +1,17 @@
+/*
+ * Description: Mostra todos os número entre 0 e 1000.
+ * Author: Prof. Lucas
+ * Created at: 12/06/2023
+ * Updated at: 12/06/2023
+ */
+
+import java.util.Scanner;
+
+public class LucasSouza_Exercicio06{
+    public static void main(String[] args){
+        /* MOSTRA TODOS OS NÚMEROS DE 0 A 1000 */
+            for(int i = 0; i <= 1000; i++){
+                System.out.println(i);
+            }
+    }
+}
