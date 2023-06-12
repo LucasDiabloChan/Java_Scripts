cff6ae53b4eb21f54612e27eca4397d550cc1e26
Diablo <82063404+LucasDiabloChan@users.noreply.github.com>
2023-06-12 09:55:37 -0300

    Update LucasSouza_Exercicio02.java
    l

--- LucasSouza_TerceiraLista/LucasSouza_Exercicio02.java
+++ LucasSouza_TerceiraLista/LucasSouza_Exercicio02.java
@@ -1,5 +1,5 @@
 /*
- * Description: Mostra todos os número entre 0 e 1000.
+ * Description: Mostra todos os número entre dois valores inseridos.
  * Author: Prof. Lucas
  * Created at: 12/06/2023
  * Updated at: 12/06/2023
@@ -7,11 +7,40 @@
 
 import java.util.Scanner;
 
-public class LucasSouza_Exercicio06{
+public class LucasSouza_Exercicio02{
     public static void main(String[] args){
+        
+        /* CRIAÇÃO DAS VARIÁVEIS */
+            final Scanner reader;
+            int primeiroValor, segundoValor;
+        
+        /* INICIALIZAÇÃO DAS VARIÁVEIS */
+            reader = new Scanner(System.in);
+            primeiroValor = segundoValor = 0;    
+        
+        /* RECEBE OS VALORES */
+            System.out.println("Insira um número: ");
+                primeiroValor = reader.nextInt();
+            System.out.println("Insira outro número: ");
+                segundoValor = reader.nextInt();
+        
         /* MOSTRA TODOS OS NÚMEROS DE 0 A 1000 */
-            for(int i = 0; i <= 1000; i++){
-                System.out.println(i);
+            if(primeiroValor < segundoValor){
+               // O primeiro valor é maior
+                    for(int i = (primeiroValor + 1); i < segundoValor; i++){
+                        System.out.println(i);
+                    }
+                
+            }else if(segundoValor < primeiroValor){
+                // O segundo valor é maior
+                    for(int i = (segundoValor + 1); i < primeiroValor; i++){
+                        System.out.println(i);
+                    }
+                
+            }else{
+                
+                // Os valores são iguais
+                System.out.println("Valores iguais não contam bobão.");
             }
     }
 }
