240aec0ab861266ecffaabcb47c8c44a0e7f04a3
Diablo <82063404+LucasDiabloChan@users.noreply.github.com>
2023-06-27 14:25:48 -0300

    Update LucasSouza_Exercicio01.java

--- LucasSouza_QuartaLista/LucasSouza_Exercicio01.java
+++ LucasSouza_QuartaLista/LucasSouza_Exercicio01.java
@@ -1,1 +1,41 @@
+/*
+ * Description: Exibe a soma de 4 valores
+ * Author: Prof. Lucas
+ * Created at: 10 jun. 2023
+ * Updtated at: 10 jun. 2023
+ */
 
+package LucasSouza_QuartaLista;
+
+import java.util.Scanner;
+
+public class LucasSouza_Exercicio01{
+	public static void main(String[] args){
+// - - - - - - - - - - - - - - - - - - - - - - - - - - //
+
+		// Declaração
+		    Scanner leitor;
+			int limiteDosNumeros, somaDasBases;
+			
+        // Inicialização
+            leitor = new Scanner(System.in);
+            somaDasBases = 0;
+            
+// - - - - - - - - - - - - - - - - - - - - - - - - - - //
+
+        System.out.println("Insira um número: ");
+		limiteDosNumeros = leitor.nextInt();
+        
+		for(int i = 1; i <= limiteDosNumeros; i++){
+			int baseAoQuadrado = i * i;
+
+			if(baseAoQuadrado % 4 == 0)
+				somaDasBases += i;		
+		}
+		
+		System.out.println("Soma das bases: ");
+		System.out.println(somaDasBases);
+		
+// - - - - - - - - - - - - - - - - - - - - - - - - - - //
+	}
+}
